e9dee947f5785c06c1235ffd026a9a90bd652845
robindimyan <robindimyan@gmail.com>
2017-01-06 05:14:35 +0200

    Create anti-hook.c

---
+++ anti-hook/anti-hook.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void check_hook()
+{
+	if (getenv("LD_PRELOAD") != NULL) // LD_PRELOAD değişkeninin içi doluysa bu function hook yapıldığına işaret.
+		exit(0); // Bu durumda programı kapatalım.
+}
+
+int main()
+{
+	check_hook();
+	puts("No hooks detected! Starting evil stuff..");
+
+	return 0;
+}
