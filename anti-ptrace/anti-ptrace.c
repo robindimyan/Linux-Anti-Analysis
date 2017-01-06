b526f2ea24287ed5c46d7786c05b7c4662f7d710
robindimyan <robindimyan@gmail.com>
2017-01-06 04:52:50 +0200

    Create anti-ptrace.c

---
+++ anti-ptrace/anti-ptrace.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ptrace.h>
+
+int check_ptrace()
+{
+	if ( ptrace(PTRACE_TRACEME) == -1 ) // Program kendisine "attach" olamadığı takdirde bu debugger kullanıldığına işaret edebilir.
+		exit(0);
+}
+
+int main()
+{
+	check_ptrace();
+	puts("No ptrace detected! Starting evil stuff..");
+	
+	return 0;
+}
