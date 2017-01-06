e2f2ddbdf29c41b2823b035ffb3f53e85ff1f0ac
robindimyan <robindimyan@gmail.com>
2017-01-06 15:29:45 +0200

    Update anti-vpc.c

--- anti-virtualpc/anti-vpc.c
+++ anti-virtualpc/anti-vpc.c
@@ -20,20 +20,20 @@ void check_vm()
 	char * code = {     // Çalıştırılacak shellcode.
 		"\x90\x90\x90\x90"
 		"\x0f\x3f\x07\x0b" // Bu satırdaki değerler VirtualPC'ye özel opcode değerleri.
-		"\xc3"             // Bunları normalde çalıştırmayı denediğinizde segmentation-fault hatasıyla karşılaşırsınız.
+		"\xc3"             // Bunları normalde çalıştırmayı denediğinizde Illegal Instruction hatasıyla karşılaşırsınız.
 	};
 
 	void (*fPtr)(void); // Fonksiyon pointer tanımladık.
 
 	fPtr = mmap(NULL, 4096, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0); // Fonksiyonumuz için hafızadan yer ayırdık ve bu alana RWX izinlerini verdik.
-	memcpy(fPtr, code, 5); // Shellcode'umuzu bu tahsis edilen alana kopyaladık.
+	memcpy(fPtr, code, 9); // Shellcode'umuzu bu tahsis edilen alana kopyaladık.
 
-	fPtr(); // Ardından fonksiyonumuzu çağırdık. Bu kod Virtual PC dışında biyerde çalıştırıldığında Segmentation Fault hatası verecek.
+	fPtr(); // Ardından fonksiyonumuzu çağırdık. Bu kod Virtual PC dışında biyerde çalıştırıldığında Illegal Instruction hatası verecek.
 }
 
 int main()
 {
-	signal( SIGSEGV, catch_segfault); // Segmentation fault signali'ni dinlemeye alıyoruz.
+	signal( SIGILL, catch_segfault); // Illegal instruction signali'ni dinlemeye alıyoruz.
 	check_vm(); // Virtual PC içerisindemiyiz bakalım..
 	puts("Virtual PC detected.."); // Şayet Virtual PC'de çalışıyorsak shellcode başarıyla çalışacak dolayısı ile hata üretmeyecek.
 	return 0; // Bu durumda da herhangi bir işlem yapmadan programı kapatalım.
