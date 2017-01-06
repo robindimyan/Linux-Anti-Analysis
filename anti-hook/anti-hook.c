a22f9679eea674d91dfd1a040845260fa5a8b759
robindimyan <robindimyan@gmail.com>
2017-01-06 05:14:53 +0200

    Update anti-hook.c

--- anti-hook/anti-hook.c
+++ anti-hook/anti-hook.c
@@ -3,7 +3,7 @@
 
 void check_hook()
 {
-	if (getenv("LD_PRELOAD") != NULL) // LD_PRELOAD değişkeninin içi doluysa bu function hook yapıldığına işaret.
+	if (getenv("LD_PRELOAD") != NULL) // LD_PRELOAD ortam değişkeninin içi doluysa bu function hook yapıldığına işaret.
 		exit(0); // Bu durumda programı kapatalım.
 }
 
